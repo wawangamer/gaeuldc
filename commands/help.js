a67b8642295484e12b128b00725519b47226fe98
Shouko Sagiri <99956524+shoukosagiri-poi@users.noreply.github.com>
2022-05-26 00:39:24 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -37,9 +37,7 @@ module.exports = {
       ).setDescription(`${Commands.join("\n")}
   
   Discord Music Bot Version: v${require("../package.json").version}
-  [✨ Support Server](${
-    client.botconfig.SupportServer
-  }) | [GitHub](https://github.com/SudhanPlayz/Discord-MusicBot) | [Dashboard](${client.botconfig.Website}) | By [SudhanPlayz](https://github.com/SudhanPlayz)`);
+[Dashboard](${client.botconfig.Website}) | By [Shouko.Sagiri](https://www.instagram.com/shoukosagiri.poi)`);
     if (!args[0]) message.channel.send(Embed);
     else {
       let cmd =
